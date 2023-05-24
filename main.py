a049a7a98a97149a6e622e6aeb88dfb021fc40e7
Justin <justin.warambourg@gmail.com>
2023-05-24 17:39:51 +0200

    facilitate testing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from source.parse_pinyin import PinyinUtils
 from source.pinyin_to_zhuyin import bopomofo
-input_str = "Hūnyīn jièshào jīgòu dōu shì wéifǎ de"
+input_str = "Lingguo dàchīyìjīng!"
 
 PinUti = PinyinUtils()
 
@@ -11,4 +11,5 @@ pinyin_tone_mark_list = PinUti.tokenize_pinyin_text(input_str)
 pinyin_tone_number_list = [PinUti.pinyin_diacritics_to_decimals(item) for item in pinyin_tone_mark_list]
 print(pinyin_tone_number_list)
 bopomofo_list = [bopomofo(syllable) for syllable in pinyin_tone_number_list]
-print(bopomofo_list)
+bopomofo_list
+print(" ".join(bopomofo_list))
